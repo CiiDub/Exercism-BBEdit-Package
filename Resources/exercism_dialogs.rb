447f58b7c7a6debce32e2ebc428e794610fd0627
Christopher Waterman <chris@rustydogink.com>
2024-07-07 12:53:16 -0700

    Replaced IO.popen with Open3.capture2e.

--- Resources/exercism_dialogs.rb
+++ Resources/exercism_dialogs.rb
@@ -19,14 +19,14 @@ module DialogBuilder
 		default_items_list = make_list_string.call( default_items )
 		multi = multiselect ? ' with multiple selections allowed' : ''
 		chooser_script = "set picked to choose from list {#{items_list}} with prompt \"#{prompt}\" default items {#{default_items_list}}#{multi}"
-		IO.popen( ['osascript', '-e', chooser_script.to_s, { err: [:child, :out] }] ).read
+		Open3.capture2e( 'osascript', '-e', chooser_script )[0]
 	end
 
 	def display_dialog_with( title:, message:, buttons: [:default], highlighted_button: :default )
 		button_string = buttons[0] == :default ? '' : make_button_string( buttons )
 		highlighted_button_string = highlighted_button == :default ? '' : " default button #{highlighted_button}"
 		dialog_script = "display dialog \"#{message}\" with title \"#{title}\"#{button_string}#{highlighted_button_string}"
-		IO.popen( ['osascript', '-e', dialog_script.to_s, { err: [:child, :out] }] ).read
+		Open3.capture2e( 'osascript', '-e', dialog_script )[0]
 	end
 
 	def make_button_string( buttons )
