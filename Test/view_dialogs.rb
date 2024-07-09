5b41f1e0671e239ffc8bda3cd4d5847f87645301
Christopher Waterman <chris@rustydogink.com>
2024-07-09 14:17:03 -0700

    Added a file for checking if not testing dialogs.

---
+++ Test/view_dialogs.rb
@@ -0,0 +1,23 @@
+require_relative '../Resources/exercism_dialogs'
+
+module InnerDialog
+  extend ExercismDialogs
+end
+
+# InnerDialog.display_clipboard_error
+
+# InnerDialog.display_download_confirmation
+
+# InnerDialog.display_download_error
+
+# InnerDialog.display_outside_workspace_error
+
+# InnerDialog.display_overwrite_confirmation
+
+# InnerDialog.display_upload_error
+
+# InnerDialog.display_webpage_error
+
+# InnerDialog.exercise_chooser [{ 'track' => 'one', 'exercise' => 'foo' }, { 'track' => 'one', 'exercise' => 'bar' }]
+
+# InnerDialog.solution_chooser ['one', 'two', 'three']
