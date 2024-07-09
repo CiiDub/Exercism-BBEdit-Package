90b648b97f6f776f296d413db5c4f714628bcc95
Christopher Waterman <chris@rustydogink.com>
2024-07-09 14:22:55 -0700

    Added a BBEditStyleLogWriter#clean_whitespace method to format text for logs and dialogs.

--- Resources/exercism_download.rb
+++ Resources/exercism_download.rb
@@ -22,7 +22,7 @@ module ExercismDownload
   def download_exercise( workspace, track, exercise, force: false )
     overwrite = force ? '--force' : ''
     message   = Open3.capture2e( 'exercism', 'download', overwrite, "--track=#{track}", "--exercise=#{exercise}" )[0]
-    display_download_error( message ) unless /^Downloaded to/.match? message
+    display_download_error( BBEditStyleLogWriter.clean_whitespace( message )) unless /^Downloaded to/.match? message
 
     open_downloaded( workspace, track, exercise )
   end
