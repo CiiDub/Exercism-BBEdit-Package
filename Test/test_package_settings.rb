b14d53062ceba74c4659c7f703c370c40e6d4cfb
Christopher Waterman <chris@rustydogink.com>
2024-07-23 13:53:56 -0700

    Change to test_package_settings; make_plist to plist_text.

--- Test/test_package_settings.rb
+++ Test/test_package_settings.rb
@@ -2,7 +2,7 @@ require 'minitest/autorun'
 require 'tempfile'
 require_relative '../Resources/package_settings'
 
-def make_plist( tag_name: '', save_on_test: 0, save_on_submit: 0 )
+def plist_text( tag_name: '', save_on_test: 0, save_on_submit: 0 )
   <<~PLIST
     <?xml version="1.0" encoding="UTF-8"?>
     <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
@@ -30,37 +30,37 @@ describe 'Setting' do
   end
 
   it '#tag_on_test should return "tag_name"' do
-    @plist << make_plist( tag_name: 'tag_name' )
+    @plist << plist_text( tag_name: 'tag_name' )
     @plist.rewind
     expect Settings.tag_on_test == 'tag_name'
   end
 
   it '#tag_on_test should return false' do
-    @plist << make_plist
+    @plist << plist_text
     @plist.rewind
     expect Settings.tag_on_test == false
   end
 
   it '#autosave_on_test? should return true' do
-    @plist << make_plist( save_on_test: '1' )
+    @plist << plist_text( save_on_test: '1' )
     @plist.rewind
     expect Settings.tag_on_test == true
   end
 
   it '#autosave_on_test? should return false' do
-    @plist << make_plist
+    @plist << plist_text
     @plist.rewind
     expect Settings.tag_on_test == false
   end
 
   it '#autosave_on_submit? should return true' do
-    @plist << make_plist( save_on_test: '1' )
+    @plist << plist_text( save_on_test: '1' )
     @plist.rewind
     expect Settings.tag_on_test == true
   end
 
   it '#autosave_on_submit? should return false' do
-    @plist << make_plist
+    @plist << plist_text
     @plist.rewind
     expect Settings.tag_on_test == false
   end
